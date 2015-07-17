591caa59999b0ad1a9ae8aded488effc1164f574
Chenyang GAO <chgao-ext@airfrance.fr>
2015-07-17 17:53:06 +0200

    page de test pour glisser

--- stkgg/WebContent/app/scripts/module/user/controller/loginCtrl.js
+++ stkgg/WebContent/app/scripts/module/user/controller/loginCtrl.js
@@ -4,6 +4,23 @@
 	
 	mod.controller("LoginCtrl", ['$scope', function($scope){
 		
+		$(window).scroll(function () {
+			console.log($("#cc").offset().top+", "+$(window).scrollTop()+", "+($("#cc").offset().top - $(window).scrollTop()));
+			
+			var eTop = $('#aa').offset().top;
+			var scrolltopAA = eTop - $(window).scrollTop();
+			if(scrolltopAA<=0){
+				$("#bb").css({"position": "fixed", "top":"50px"});
+				$("#cc").css({"position": "absolute", "top":"250px"});
+				
+			}else{
+				$("#bb").css({"position": "relative", "top":"0"});
+				$("#cc").css({"position": "relative", "top":"0"});
+			}
+		});
+		
+		
+		
 	}]);
 
 })();
