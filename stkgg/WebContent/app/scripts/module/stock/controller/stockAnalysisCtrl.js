e85e8ac9ce356717638f7e2135ad280939df9605
chenyang <chenyanggao123@gmail.com>
2015-07-19 21:22:51 +0200

    ajout plus mock cmt dans stkAnal

--- stkgg/WebContent/app/scripts/module/stock/controller/stockAnalysisCtrl.js
+++ stkgg/WebContent/app/scripts/module/stock/controller/stockAnalysisCtrl.js
@@ -3,7 +3,8 @@
 	var mod = angular.module('stock.controller');
 	mod.controller("StockAnalysisCtrl", ['$scope', function($scope){
 
-		$scope.repeatCommentBlock = ['1', '2', '3', '4', '5'];
+		$scope.repeatCommentBlock = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 
+		                             '11', '12', '13', '14', '15', '16', '17', '18', '19', '20'];
 
 		$scope.options = {
 				chart: {
