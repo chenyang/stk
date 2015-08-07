165129bcf3df25a444c570ce2f8e8474ab39b1a3
GAO CHENYANG <chenyanggao123@gmail.com>
2015-08-07 14:58:25 +0800

    1. API doc update 2. change stkAnal data integration structure

--- stkgg/WebContent/app/scripts/module/stock/controller/stockAnalysisCtrl.js
+++ stkgg/WebContent/app/scripts/module/stock/controller/stockAnalysisCtrl.js
@@ -8,66 +8,71 @@
 		$scope.yucePopover = {
 				content: '预计一周内跌5%， 跌幅15点',
 		};
+		
+		$scope.pubInfo = {
+				sessionId:"IUWID987398OSI", //关联用户
+				pubId:15, //关联pub, 
+				stockId:123, //关联 stock
+				stockName: "中石油",
+				stockCode: "06160",
+				result:"success", 
+				reason: null, 
+				desc:"一段评述", 
+				owner:"股神小白", 
+				available:true,
+				prediction:{
+					time:7,
+					minVal:-2, 
+					maxVal:3.5,
+					minPect:15, 
+					maxPect:20,
+					cmt:"预测跌幅较大，建议抛售", 
+					lastModifiedTime:'2015-07-01 15:37:28'
+				},
+				tips:[
+				      {
+				    	  tipId:1, 
+				    	  newsId:2, //关联新闻
+				    	  newsTitle:"消息称国务院将特批银行持券商牌照试点", 
+				    	  newsDate:"2015-09-09",
+				    	  newsContent:"伴随着银行混合所有制改革拉开大幕，混业经营备受关注，放开券商牌照的问题如一把悬在头上的达摩克利斯之剑。日前，《经济参考报》记者从知情人士处获悉，《商业银行法》修改的内容里，除了取消存贷比外，更重要的是涉及银行经营业务范围的进一步完善和补充，给混业经营在法律层面上预留空间。银行受托理财的功能，银行不能直接做信托业务，但实际上银行理财产品已经承担了代客理财、受托投资的功能。收益与风险挂钩，不可能全是刚性兑付，而现实是投资者无法接受损失风险，这也是制度层面不完善所带来的误区。未来银行业在混业经营的趋势下，业务范围需要进一步界定。在监管方面，未来一定是倾向于根据金融机构的具体业务划定监管职责，而非根据机构本身来划定监管，因此协同监管也越来越重要。”鲁政委说。", 
+				    	  tipContent:"持续的资金注入难改短期的弱势格局1， 便再次出现大跌", 
+				    	  lastModifiedTime:"2015-07-01 16:26", //最后修改时间
+				    	  isCollapsedShare:true, 
+				    	  isCollapsedCmt:true,
+				    	  isModifyTip:false
+				      }, 
+				      {
+				    	  tipId:2, 
+				    	  newsId:1, //关联新闻
+				    	  newsTitle:"消息称国务院将特批银行持券商牌照试点", 
+				    	  newsDate:"2015-09-09",
+				    	  newsContent:"伴随着银行混合所有制改革拉开大幕，混业经营备受关注，放开券商牌照的问题如一把悬在头上的达摩克利斯之剑。日前，《经济参考报》记者从知情人士处获悉，《商业银行法》修改的内容里，除了取消存贷比外，更重要的是涉及银行经营业务范围的进一步完善和补充，给混业经营在法律层面上预留空间。银行受托理财的功能，银行不能直接做信托业务，但实际上银行理财产品已经承担了代客理财、受托投资的功能。收益与风险挂钩，不可能全是刚性兑付，而现实是投资者无法接受损失风险，这也是制度层面不完善所带来的误区。未来银行业在混业经营的趋势下，业务范围需要进一步界定。在监管方面，未来一定是倾向于根据金融机构的具体业务划定监管职责，而非根据机构本身来划定监管，因此协同监管也越来越重要。”鲁政委说。", 
+				    	  tipContent:"持续的资金注入难改短期的弱势格局1， 便再次出现大跌", 
+				    	  lastModifiedTime:"2015-07-01 16:26", //最后修改时间
+				    	  isCollapsedShare:true, 
+				    	  isCollapsedCmt:true,
+				    	  isModifyTip:false
+				      }, 
+				      {
+				    	  tipId:3, 
+				    	  newsId:2, //关联新闻
+				    	  newsTitle:"消息称国务院将特批银行持券商牌照试点", 
+				    	  newsDate:"2015-09-09",
+				    	  newsContent:"伴随着银行混合所有制改革拉开大幕，混业经营备受关注，放开券商牌照的问题如一把悬在头上的达摩克利斯之剑。日前，《经济参考报》记者从知情人士处获悉，《商业银行法》修改的内容里，除了取消存贷比外，更重要的是涉及银行经营业务范围的进一步完善和补充，给混业经营在法律层面上预留空间。银行受托理财的功能，银行不能直接做信托业务，但实际上银行理财产品已经承担了代客理财、受托投资的功能。收益与风险挂钩，不可能全是刚性兑付，而现实是投资者无法接受损失风险，这也是制度层面不完善所带来的误区。未来银行业在混业经营的趋势下，业务范围需要进一步界定。在监管方面，未来一定是倾向于根据金融机构的具体业务划定监管职责，而非根据机构本身来划定监管，因此协同监管也越来越重要。”鲁政委说。", 
+				    	  tipContent:"持续的资金注入难改短期的弱势格局1， 便再次出现大跌", 
+				    	  lastModifiedTime:"2015-07-01 16:26", //最后修改时间
+				    	  isCollapsedShare:true, 
+				    	  isCollapsedCmt:true,
+				    	  isModifyTip:false
+				      }
+				      ]
+		};
 
 		//查看我的预测
 		$scope.modifyMyPrediction = function(){
 
 		};
-
-		//repeated parts
-		$scope.items = 
-			[
-			 {
-				 userId:1,
-				 tipId:1, 
-				 tipContent:"持续的资金注入难改短期的弱势格局1， 便再次出现大跌，这就是我们所说的趋势的力量", 
-				 lastModifiedTime:"2015-07-01 16:26", 
-				 isCollapsedShare:true, 
-				 isCollapsedCmt:true,
-				 isModifyTip:false, 
-				 prediction:null, 
-				 bought:true //free or bought
-			 }, 
-			 {
-				 userId:1,
-				 tipId:2, 
-				 tipContent:"持续的资金注入难改短期的弱势格局2，便再次出现大跌，这就是我们所说的趋势的力量", 
-				 lastModifiedTime:"2015-07-01 16:26", 
-				 isCollapsedShare:true, 
-				 isCollapsedCmt:true,
-				 isModifyTip:false, 
-				 prediction:{
-					 time:7, 
-					 minVal:-2, 
-					 maxVal:3.5, 
-					 minPect:15, 
-					 maxPect:20, 
-					 cmt:"预测跌幅较大，建议抛售", 
-					 lastModifiedTime:'2015-07-01 15:37:28'
-				 },
-				 bought:false
-			 },
-			 {
-				 userId:1,
-				 tipId:3, 
-				 tipContent:"持续的资金注入难改短期的弱势格局3，便再次出现大跌，这就是我们所说的趋势的力量", 
-				 lastModifiedTime:"2015-07-01 16:26", 
-				 isCollapsedShare:true, 
-				 isCollapsedCmt:true,
-				 isModifyTip:false, 
-				 prediction:{
-					 time:1, 
-					 minVal:0.5, 
-					 maxVal:1.5, 
-					 minPect:5, 
-					 maxPect:8,
-					 cmt:"预测跌幅较大，建议抛售", 
-					 lastModifiedTime:'2015-07-01 15:37:28'
-				 },
-				 bought:true
-			 }
-
-			 ];
 
 		$scope.modifyTip = function(item){
 			item.isModifyTip = !item.isModifyTip;
@@ -77,8 +82,9 @@
 		};
 
 		$scope.pubDetail = function(item){
+			item.available = $scope.pubInfo.available;
 			//not yet bought
-			if(!item.bought){
+			if(!item.available){
 				$location.path("/buyCmt");
 			}else{
 				//check if tipId is already visited
